74afcc515f8558f102cccb9a5f83cec4fc8654d7
Bryan Soltis <bryan@soltisweb.com>
2019-01-23 15:28:15 -0600

    ADMIN SITE - Created custom page types - Created Home page content item - Generated custom page type classes - Enabled CI - Included CIRepostory folder
    MVC SITE
    - Created Home controller
    - Created Home view
    - Created SHared/_layout file

---
+++ KenticoMVCWidgetShowcase/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using CMS.DocumentEngine.Types.KenticoMVCWidgetShowcase;
+using System.Web.Mvc;
+
+namespace KenticoMVCWidgetShowcase.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            // Gets the latest version of a single article using the generated provider
+            HomePage page = HomePageProvider.GetHomePage("/home", "en-us", "KenticoMVCWidgetShowcase");
+
+            return View(page);
+        }
+    }
+}
